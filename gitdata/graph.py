5270a5fc85c4f0bf499027fb056e7822c5923da4
Herb Lainchbury <herb@dynamic-solutions.com>
2019-06-11 06:08:21 -0700

    add add method to Graph

--- gitdata/graph.py
+++ gitdata/graph.py
@@ -2,11 +2,16 @@
     gitdata graph
 """
 
+import gitdata
+from gitdata.utils import new_test_uid
+
+
 class Graph(object):
     """Basic Graph"""
 
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, store):
+        self.store = store
+        self.digester = gitdata.digester.Digester(new_uid=new_test_uid(0))
 
     def set(self, value):
         """set one or more entity values"""
@@ -16,3 +21,13 @@ class Graph(object):
             values = iter([value])
         for value in values:
             pass
+
+    def add(self, data):
+        """Add arbitrary data to the graph"""
+        self.digester.digest(data)
+        return self.store.add(self.digester.known)
+
+    def get(self, uid):
+        """Get a node of the graph"""
+        node = self.store.get(uid)
+        return node
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -16,6 +16,7 @@ class GraphTests(unittest.TestCase):
     def setUp(self):
         store = gitdata.stores.memory.MemoryStore()
         self.graph = gitdata.Graph(store)
+        self.graph.store.clear()
 
     def test_set(self):
         g = self.graph
@@ -24,3 +25,9 @@ class GraphTests(unittest.TestCase):
         g.set(dict(name='joe'))
         g.set([1, 'x', Decimal('2.22')])
         g.set([dict(name='Joe', age=20), dict(name='Pat', age=24)])
+
+    def test_add(self):
+        g = self.graph
+        g.add([dict(name='Joe', age=20), dict(name='Pat', wage=Decimal('12.1'))])
+        person = g.get(3)
+        self.assertEqual(person['wage'], Decimal('12.1'))
