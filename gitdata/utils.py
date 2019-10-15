597038dbfd62e8d1d6fa470cf2dd3c649b625861
Herb Lainchbury <herb@dynamic-solutions.com>
2019-10-14 17:12:43 -0700

    add trim function

--- gitdata/utils.py
+++ gitdata/utils.py
@@ -51,3 +51,81 @@ def as_uri(location):
     """Returns a local file system location as a file URI (rfc8089)"""
     return 'file://{}{}'.format(platform.node(), os.path.realpath(location))
 
+
+def trim(text):
+    """Remove excess spaces from a block of text
+
+    >>> trim('remove right ')
+    'remove right'
+
+    >>> trim(' remove left')
+    'remove left'
+
+    >>> print(trim(' remove spaces\\n    from block\\n    of text'))
+    remove spaces
+       from block
+       of text
+
+    >>> print(
+    ...     trim(
+    ...     '    \\n'
+    ...     '    remove spaces\\n'
+    ...     '        from block\\n'
+    ...     '        of text\\n'
+    ...     '    \\n'
+    ...     '\\n'
+    ...     )
+    ... )
+    <BLANKLINE>
+    remove spaces
+        from block
+        of text
+    <BLANKLINE>
+    <BLANKLINE>
+
+    >>> print(trim('    remove spaces\\n  from block\\n  of text\\n    '))
+      remove spaces
+    from block
+    of text
+    <BLANKLINE>
+
+    >>> print(trim('    remove spaces\\n  from block\\n  of text'))
+      remove spaces
+    from block
+    of text
+
+    >>> print(trim('\\n  remove spaces\\n    from block\\n  of text'))
+    <BLANKLINE>
+    remove spaces
+      from block
+    of text
+
+    >>> text = '\\nremove spaces  \\n    from block\\nof text'
+    >>> print('\\n'.join(repr(t) for t in trim(text).splitlines()))
+    'remove spaces  '
+    '    from block'
+    'of text'
+
+    >>> text = (
+    ...     '\\nremove spaces'
+    ...     '\\n    from block'
+    ... )
+    >>> print(trim(text))
+    remove spaces
+        from block
+
+    """
+    trim_size = None
+    lines = text.splitlines()
+    for line in lines:
+        if not line or line.isspace():
+            continue
+        n = len(line) - len(line.lstrip())
+        trim_size = min([trim_size, n]) if trim_size is not None else n
+    if trim_size:
+        result = []
+        for line in lines:
+            result.append(line[trim_size:])
+        return '\n'.join(result)
+    else:
+        return text.strip()
